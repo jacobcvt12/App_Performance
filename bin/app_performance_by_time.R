e3e6cbf3c3c21067500854d1b2f67a867f0dcd25
Jacob Carey <jacob.carey@marriott.com>
2014-02-19 18:13:12 -0500

    Start new R file. Create test shell script

---
+++ bin/app_performance_by_time.R
@@ -0,0 +1,15 @@
+library(RSQLite)
+library(scales)
+db <- "~/marriott/App_Performance/data/reviews.db"
+
+conn <- dbConnect("SQLite", db)
+weekly_ratings_df <- dbGetQuery(conn, "SELECT * FROM ratings_by_week;")
+weekly_ratings_df$week_end <- as.Date(weekly_ratings_df$week_end, "%Y-%m-%d")
+
+wr_df_subset = subset(weekly_ratings_df, company %in% c("marriott", "hilton"))
+
+p <- ggplot(wr_df_subset, aes(week_end, review_avg, group=company, colour=company, linetype=company)) + geom_line() + scale_x_date(labels=date_format("%b %y"))
+print(p)
+# marriott <- subset(weekly_ratings_df, company=="marriott")
+# p <- ggplot(marriott, aes(week_end, review_avg)) + geom_line() + geom_smooth(method="lm") + scale_x_date(labels=date_format("%b %y"))
+# q <- ggplot(marriott, aes(week_end, review_count)) + geom_line() + geom_smooth(method="lm") + scale_x_date(labels=date_format("%b %y"))
--- test.sh
+++ test.sh
@@ -10,3 +10,20 @@ for animal in ${!animals[*]};
 do
 	echo ${animals[$animal]};
 done
+
+
+file=$(mktemp)
+progress() {
+  pc=0;
+  while [ -e $file ]
+    do
+      echo -ne "$pc sec\033[0K\r"
+      sleep 1
+      ((pc++))
+    done
+}
+progress &
+sleep 15
+
+#now when everything is done
+rm -f $file
