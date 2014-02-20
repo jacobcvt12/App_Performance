e18fdd3885dfd69db8c71a82822325e43471d7f6
Jacob Carey <jacob.carey@marriott.com>
2014-02-20 10:32:43 -0500

    edit test.sh

--- test.sh
+++ test.sh
@@ -4,26 +4,30 @@ declare -A animals
 
 animals=(
 [cow]=moo 
-[fox]=kekeke)
+[fox]=kekeke
+[cat]=meow
+[dog]=bark)
 
 for animal in ${!animals[*]};
 do
 	echo ${animals[$animal]};
 done
 
+echo ""
+echo "${!animals[0]}"
 
-file=$(mktemp)
-progress() {
-  pc=0;
-  while [ -e $file ]
-    do
-      echo -ne "$pc sec\033[0K\r"
-      sleep 1
-      ((pc++))
-    done
-}
-progress &
-sleep 15
-
-#now when everything is done
-rm -f $file
+#file=$(mktemp)
+#progress() {
+#  pc=0;
+#  while [ -e $file ]
+#    do
+#      echo -ne "$pc sec\033[0K\r"
+#      sleep 1
+#      ((pc++))
+#    done
+#}
+#progress &
+#sleep 15
+#
+##now when everything is done
+#rm -f $file
