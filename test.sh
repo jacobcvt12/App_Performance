9286bebfbf9faa4827e46c5e3eaf7f693ea9395c
Jacob Carey <jacobcvt12@gmail.com>
2014-02-19 20:46:48 -0500

    Add timer to main.sh

--- test.sh
+++ test.sh
@@ -1,29 +1,34 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
-declare -A animals
+#declare -A animals
 
-animals=(
-[cow]=moo 
-[fox]=kekeke)
+#animals=(
+#[cow]=moo 
+#[fox]=kekeke)
 
-for animal in ${!animals[*]};
-do
-	echo ${animals[$animal]};
-done
+#for animal in ${!animals[*]};
+#do
+	#echo ${animals[$animal]};
+#done
 
 
 file=$(mktemp)
 progress() {
-  pc=0;
-  while [ -e $file ]
+    pc=0;
+    while [ -e $file ]
     do
-      echo -ne "$pc sec\033[0K\r"
-      sleep 1
-      ((pc++))
+        if [ "$pc" == "0" ]; then
+            printf " User Time: 00:00:00\r"
+        else
+            printf " User Time: %02d:%02d:%02d\r" $((pc/3600)) $((pc/60%60)) $((pc % 60))
+        fi
+
+        sleep 1
+        ((pc++))
     done
 }
 progress &
-sleep 15
+sleep 5
 
 #now when everything is done
 rm -f $file
